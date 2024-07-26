0fdc7ad14278f1023e87cb5094d055e8a128765e
alexp230 <apruitt230@gmail.com>
2024-07-26 16:28:18 -0500

    Added PaySlip view; Plan on touching it up more later

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc, asc
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
 from flask_wtf import FlaskForm
@@ -7,19 +8,18 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_migrate import Migrate
 from datetime import datetime
+import email_validator
+import random
+import math
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite3'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 migrate = Migrate(app, db)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +32,37 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class PayRoll(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    payRate = db.Column(db.Float)
+    payPeriod = db.Column(db.Float)
+    YTD = db.Column(db.Float)
+    hours = db.Column(db.Float)
+    gross = db.Column(db.Float)
+    tax = db.Column(db.Float)
+    OT = db.Column(db.Float)
+    net = db.Column(db.Float)
+
+    def __init__(self, username, payRate, payPeriod, YTD, hours, gross, tax, OT, net):
+        self.username = username
+        self.payRate = payRate
+        self.payPeriod = payPeriod
+        self.YTD = YTD
+        self.hours = hours
+        self.gross = gross
+        self.tax = tax
+        self.OT = OT
+        self.net = net
+
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -129,5 +160,67 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
 
+@app.route("/PayView")
+def PayView():
+    if (not current_user):
+        return redirect(url_for("login"))
+    
+    userName = current_user.username
+    
+    paySlip = PayRoll.query.filter(PayRoll.username == userName).first()
+    if (not paySlip):
+        rate = round(random.uniform(20, 35), 2)
+
+        newEmployeePaySlip = PayRoll(userName, rate, 1.0, 0, 0, 0, 0, 0, 0)
+        paySlip = newEmployeePaySlip
+
+        db.session.add(newEmployeePaySlip)
+        db.session.commit()
+    
+    else:
+        paySlip = PayRoll.query.filter(PayRoll.username == userName).order_by(desc(PayRoll.payPeriod)).first()
+
+    payRate = paySlip.payRate
+
+    workedHours = round(random.uniform(40, 55), 2)
+    overTimeHours = workedHours - 40
+
+    OTpay = (payRate * 1.5 * overTimeHours)
+    OTpay = round(OTpay, 2)
+
+    grossPay = ((workedHours * payRate) + OTpay)
+    grossPay = round(grossPay, 2)
+
+    taxPay = grossPay * 0.15
+    taxPay = round(taxPay, 2)
+
+    netPay = grossPay - taxPay
+    netPay = round(netPay, 2)
+
+    YTDamount = paySlip.YTD + netPay
+    YTDamount = round(YTDamount, 2)
+
+    payPeriod = calcPayPeriod(paySlip.payPeriod)
+
+    newPaySlip = PayRoll(userName, payRate, payPeriod, YTDamount, workedHours, grossPay, taxPay, OTpay, netPay)
+    db.session.add(newPaySlip)
+    db.session.commit()
+
+    allPaySlips = PayRoll.query.filter(PayRoll.username == userName).all()
+
+    return render_template("PayView.html", pays=allPaySlips)
+
+
+def calcPayPeriod(period: float):
+    period = period + 0.01
+    period = round(period, 2)
+    test = math.ceil(period)
+
+    if (test-period <= 0.48):
+        return test
+    else:
+        return period
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
