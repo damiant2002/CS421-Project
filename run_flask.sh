d10699e1755e158268ce8f2ac196a8070db5f99a
damiant2002 <damiantucker2002@gmail.com>
2024-08-02 13:10:18 -0500

    fixed .sh

--- run_flask.sh
+++ run_flask.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+python3 -m venv venv
+
 source venv/bin/activate
 
 pip install Flask Flask-WTF Flask-Bcrypt Flask-Login Flask-SQLAlchemy Flask-Migrate email-validator werkzeug
+
+mv app.py venv/app.py
+mv static venv/static
+mv templates venv/templates
+
+cd venv
 
 flask db init
 flask db migrate -m "Initial migration."
